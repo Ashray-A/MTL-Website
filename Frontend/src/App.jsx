ca88637106523983e9331f6f2fbe9be5c31b78ea
Ashray A <127750953+Ashray-A@users.noreply.github.com>
2024-08-20 23:50:22 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,20 +4,22 @@ import './App.css'
 import ContactUs from './pages/ContactUs';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import { Button } from '@chakra-ui/react'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const click = () => {console.log('test')}
   return (
-    <div>
+    <>
       <Router>
         <Navbar/>
+        <Button colorScheme='blue' onClick={click}>Button</Button>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/contact-us" element={<ContactUs/>}/>
         </Routes>
       </Router>
-    </div>
+    </>
   )
 }
 
